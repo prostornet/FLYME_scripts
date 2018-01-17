744367e0d7c9f47889efb98e2ee8d7add8224e78
prostornet <prostornet@gmail.com>
2018-01-17 15:15:12 +0300

    commit

--- M6N_script/cidm6n.sh
+++ M6N_script/cidm6n.sh
@@ -2,7 +2,7 @@
 
 ##########################################
 #   Script changing ID for Meizu M6Note  #
-#      write by prostornet from 4pda     #
+#     written by prostornet from 4pda    #
 ##########################################
 set `id`
 UC=$1
@@ -27,6 +27,7 @@ if [  "${ID}" = "72151002" ]; then
     if [ "${ANS}" = "y" ]; then
         echo ". For changing [ID] to chinese 72101001 input [1]"
         echo ". For changing [ID] to chinese 72101002 input [2]"
+        echo ". For changing [ID] to chinese 72121001 input [3]"
         read NUM
         if [ "${NUM}" = "1" ]; then
         echo ". Now we are changing to chinese [ID] 72101001 ..."
@@ -45,6 +46,20 @@ if [  "${ID}" = "72151002" ]; then
         elif [ "${NUM}" = "2" ]; then
         echo ". Now we are changing to chinese [ID] 72101002 ..."
         echo -e '\x30''\x31''\x30''\x30''\x32' | dd of=/dev/block/mmcblk0p50 bs=1 seek=515 count=5 >/dev/null 2>&1
+            if [ $? -ne 0 ]; then
+                echo "."
+                echo "!!!! Error during changing ID"
+                echo "."
+                exit 0
+            else
+                echo ". We are finished, now [ID] is set to chinese."
+                echo ". Checking ..."
+                echo "."
+                cat /dev/block/mmcblk0p50
+            fi
+        elif [ "${NUM}" = "3" ]; then
+        echo ". Now we are changing to chinese [ID] 72121001 ..."
+        echo -e '\x32''\x31''\x30''\x30''\x31' | dd of=/dev/block/mmcblk0p50 bs=1 seek=515 count=5 >/dev/null 2>&1
             if [ $? -ne 0 ]; then
                 echo "."
                 echo "!!!! Error during changing ID"
@@ -103,6 +118,36 @@ elif [ "${ID}" = "72101001" ]; then
         exit 0
     fi
 elif [ "${ID}" = "72101002" ]; then
+    echo "."
+    echo ". Device with chinese [ID] - "${ID}
+    echo ". Do you want to change it to global [ID]? (y/n)"
+    read ANS
+    if [ "${ANS}" = "y" ]; then
+        echo ". Now we are changing to global [ID] ..."
+        echo -e '\x35''\x31''\x30''\x30''\x32' | dd of=/dev/block/mmcblk0p50 bs=1 seek=515 count=5 >/dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            echo "."
+            echo "!!!! Error during changing ID"
+            echo "."
+            exit 0
+        else
+        echo ". We are finished, now [ID] is set to global."
+        echo ". Checking ..."
+        echo "."
+        cat /dev/block/mmcblk0p50
+        fi
+    elif [ "${ANS}" = "n" ]; then
+        echo ". We aren't going change [ID]."
+        echo "."
+        # This line delete script from the system
+        rm -f $0
+        exit 0
+    else
+        echo ". You have selected the wrong letter."
+        echo "."
+        exit 0
+    fi
+elif [ "${ID}" = "72121001" ]; then
     echo "."
     echo ". Device with chinese [ID] - "${ID}
     echo ". Do you want to change it to global [ID]? (y/n)"
